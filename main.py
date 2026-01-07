5ff47ee5333202a16657cdb65d192a291014b75d
menhibtaunga <krishanpratapsingh42@gmail.com>
2026-01-07 05:45:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import asyncio
 from bot import Bot
 
-Bot().run()
+async def main():
+    bot = Bot()
+    await bot.start()
+    await asyncio.Event().wait()  # process ko zinda rakhega
+
+if __name__ == "__main__":
+    asyncio.run(main())
